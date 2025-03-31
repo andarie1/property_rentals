e8038e267c0461bb9c91efae9410179bc219a97e
andarie1 <elena.arhi88@gmail.com>
2025-03-31 22:15:53 +0300

    Changes

--- property_rental/rental_system/asgi.py
+++ property_rental/rental_system/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for rental_system project.
+ASGI config for property_rental project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rental_system.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'property_rental.settings')
 
 application = get_asgi_application()
--- property_rental/rental_system/urls.py
+++ property_rental/rental_system/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for rental_system project.
+URL configuration for property_rental project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
