ff5eccf8990d2a91337ccf04a9e9cd61736fd1b6
andarie1 <elena.arhi88@gmail.com>
2025-03-31 23:26:21 +0300

    New addings

--- listings/views.py
+++ listings/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Listing
 
-# Create your views here.
+
+def listing_list(request):
+    listings = Listing.objects.all()
+    return render(request, 'listings.html', {'listings': listings})
+
+def home(request):
+    return render(request, 'listings.html')
